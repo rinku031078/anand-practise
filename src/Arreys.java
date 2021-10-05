93eca2718b59ffac6732f9af3a0d99eee9b5676f
rinku031078 <anandtri78@gmail.com>
2021-10-05 11:50:34 +0100

    my practise

---
+++ src/Arreys.java
@@ -0,0 +1,20 @@
+public class Arreys {
+
+    public static void main (String[] args){
+
+        String[] cars = {"volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
